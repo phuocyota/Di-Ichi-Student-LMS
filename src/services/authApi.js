f7870792dca1e43bd8028ca27f1b9b7c56e4d574
phuocyota <nhphuoca7tqtpy@gmail.com>
2026-09-03 08:48:07 +0700

    u

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,7 +1,7 @@
 import { apiRequest } from './apiClient.js';
 
 export function login(email, password) {
-  return apiRequest('/auth/login', {
+  return apiRequest('/auth/student/login', {
     method: 'POST',
     body: JSON.stringify({ email, password }),
   });
